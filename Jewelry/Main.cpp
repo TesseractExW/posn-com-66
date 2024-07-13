688f814fb1f1af1a678a312ec16efca84616bb04
jjheadshotsniper <w.pawinee17@gmail.com>
2024-07-13 13:47:12 +0700

    code for jewelry  problem

---
+++ Jewelry/Main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+int main()
+{
+	int n;
+	std::cin >> n;
+	char* str = new char[n];
+	std::cin >> str;
+
+	unsigned long int sum = 0;
+
+	for (int low = 0;low < n;low++) {
+		for (int high = low;high < n;high++) {
+			int max_ = 0;
+			int count = 0;
+			for (int i = low;i <= high;i++) {
+				if (str[i] == 'T') {
+					count++;
+				}
+				else {
+					count = 0;
+				}
+				max_ = std::max(max_, count);
+			}
+			sum += max_;
+		}
+	}
+	std::cout << sum << std::endl;
+	return 0;
+}
